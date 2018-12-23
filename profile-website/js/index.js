3186d45b04d12c214b2be40fef70861d042527bd
Petivip <44558167+Petivip@users.noreply.github.com>
2018-12-23 17:53:08 -0500

    Add files via upload

---
+++ profile-website/js/index.js
@@ -0,0 +1,17 @@
+$("document").ready(function () {
+  $("img").addClass("animated fadeInDown");
+  $("h1").addClass("animated fadeInDown");
+  $("h2").addClass("animated fadeInDown");
+  $("h3").addClass("animated fadeInDown");
+  $("h4").addClass("animated fadeInDown");
+  $("h5").addClass("animated fadeInDown");
+  $("h6").addClass("animated fadeInDown");
+  $("p").addClass("animated fadeInDown");
+  $("hr").addClass("animated fadeInDown");
+  $("i").addClass("animated fadeInDown");
+  $("section").addClass("animated fadeInDown");
+  $("article").addClass("animated fadeInDown");
+  $("ul").addClass("animated fadeInDown");
+  
+  $.scrollIt();
+});
